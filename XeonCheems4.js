139d84c59870e1135341220cc91fc02371aa903d
XMatsunichi <90054984+XMatsunichi@users.noreply.github.com>
2022-08-12 10:18:03 +0800

    Update XeonCheems4.js

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -499,7 +499,7 @@ const reply = (teks) => {
         }
 
         //Push Message To Console && Auto Read\\
-        if (isCmd) {
+        if (m.message) {
             console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
         }
 	
